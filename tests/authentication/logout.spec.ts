113de53261f0988793662bad1e127dd930b08214
saraffa13 <ssaraffa786@gmail.com>
2025-10-30 15:01:41 +0530

    logout handled

--- tests/authentication/logout.spec.ts
+++ tests/authentication/logout.spec.ts
@@ -1,18 +1,19 @@
 import { expect, test } from "@playwright/test";
 import { loginUser, logoutUser } from "./auth-utils";
 
-test.describe('Authentication Tests', () => {
+test.describe("Authentication Tests", () => {
+  test.setTimeout(90000);
 
-    test.beforeEach(async ({ page }) => {
-        await loginUser(page);
-        await page.waitForLoadState('networkidle');
-        await page.getByRole('button', { name: 'Dashboard' }).click();
-        await expect(page).toHaveURL(/^(?:https?:\/\/)?[^\/]*\/(?:[^\/]+\/)*dashboard(?:\/|$)/i);
-    });
+  test.beforeEach(async ({ page }) => {
+    await loginUser(page);
+    await page.getByRole("button", { name: "Dashboard" }).waitFor({ state: "visible", timeout: 60000 });
+    await page.getByRole("button", { name: "Dashboard" }).click();
+    await expect(page).toHaveURL(/dashboard/, { timeout: 60000 });
+  });
 
-    test("logout test", async ({ page }) => {
-        await logoutUser(page);
-        await expect(page).not.toHaveURL(/dashboard/); 
-        await expect(page.getByRole('button', { name: 'Dashboard' })).toBeHidden();
-    });
+  test("logout test", async ({ page }) => {
+    await logoutUser(page);
+    await expect(page).not.toHaveURL(/dashboard/);
+    await expect(page.getByRole("button", { name: "Dashboard" })).toBeHidden();
+  });
 });
