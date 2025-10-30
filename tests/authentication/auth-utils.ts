45ce42c8cfdadfd58d6bd591a90be374bc682d7b
saraffa13 <ssaraffa786@gmail.com>
2025-10-30 14:04:52 +0530

    checking

--- tests/authentication/auth-utils.ts
+++ tests/authentication/auth-utils.ts
@@ -1,7 +1,8 @@
 import { Page } from "@playwright/test";
 
-export async function loginUser(page: Page, email: string = "admin@agency1.com", password: string = "12345678") {
-  await page.goto("http://localhost:5173/login");
+export async function loginUser(page: Page, email: string = "admin1@upandup.com", password: string = "Password@123") {
+  // await page.goto("http://localhost:5173/login");
+  await page.goto("https://main.dlsxzkf1thczu.amplifyapp.com");
   await page.getByRole("textbox", { name: "Email" }).click();
   await page.getByRole("textbox", { name: "Email" }).fill(email);
   await page.getByRole("textbox", { name: "Email" }).press("Tab");
